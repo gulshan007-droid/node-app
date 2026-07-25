5f8a24b7ae790bc3e1027ee8d29670a54b7801a8
Gulshan Verma <gulshanverma@Gulshans-MacBook-Air.local>
2026-07-25 17:47:00 +0530

    queue uri

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
   res.send("Hello World! fghh my name is reshama patel");
 });
 
+app.get("/queue", (req, res) => {
+  res.send("Queue route is not updated data");
+});
+
 app.listen(45000, () => {
   console.log("Server is running on port 45000");
 });
